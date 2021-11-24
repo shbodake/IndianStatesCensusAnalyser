1e56fc5ce06660fc8d7859e9ba0d3d9274749001
shbodake <sayalibodake28@gmail.com>
2021-11-24 17:42:26 +0530

    Added: When incorrect delimeter is added in csv file throwing custom throws exception

--- src/test/java/com/blp/stateanalyser/CensusAnalyserTest.java
+++ src/test/java/com/blp/stateanalyser/CensusAnalyserTest.java
@@ -10,6 +10,7 @@ public class CensusAnalyserTest {
     private String INDIAN_CENSUS_CSV_FILE_PATH = "H:\\IndianStatesCensusAnalyser\\src\\main\\resources\\IndianStateCensus.csv";
     private String INIDAN_CENSUS_WrongCSV_FILE_PATH = "H:\\IndianStatesCensusAnalyser\\src\\main\\resources\\IndianStateCensus1.csv";
     private String INDIAN_CENSUS_INCORRECT_FILE_FORMAT  = "H:\\IndianStatesCensusAnalyser\\src\\main\\resources\\IndianStateCensus1.txt";
+    private String INDIAN_CENSUS_WITHWRONG_DELIMITER = "H:\\IndianStatesCensusAnalyser\\src\\main\\resources\\IndianStateCensus.csv";
 
     // Tc1.1
     @Test
@@ -44,6 +45,22 @@ public class CensusAnalyserTest {
             censusAnalyser.loadIndiaCensusData(INDIAN_CENSUS_INCORRECT_FILE_FORMAT);
         } catch (CensusAnalyserException e) {
             Assert.assertEquals(CENSUS_INCORRECT_FILE_FORMAT, e.type);
+        }
+    }
+
+    // TC1.4
+    @Test
+    public void givenIndianCensusCSVFile_WhenWrongDelimiter_ShouldThrowException() {
+
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(INDIAN_CENSUS_WITHWRONG_DELIMITER);
+        }
+        catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_WRONG_DELIMITER, e.type);
+            e.printStackTrace();
         }
     }
 }
