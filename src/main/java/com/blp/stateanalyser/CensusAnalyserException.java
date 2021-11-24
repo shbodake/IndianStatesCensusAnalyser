c5d1060d2f74f430b8da17c76ce6b9c0f497891b
shbodake <sayalibodake28@gmail.com>
2021-11-24 18:01:03 +0530

    Added: When wrong header provided in csv should throw custom exception

--- src/main/java/com/blp/stateanalyser/CensusAnalyserException.java
+++ src/main/java/com/blp/stateanalyser/CensusAnalyserException.java
@@ -2,7 +2,7 @@ package com.blp.stateanalyser;
 
 public class CensusAnalyserException extends Exception{
     public enum ExceptionType {
-        CENSUS_FILE_INCORRECT, CENSUS_INCORRECT_FILE_FORMAT, CENSUS_WRONG_DELIMITER;
+        CENSUS_FILE_INCORRECT, CENSUS_INCORRECT_FILE_FORMAT, CENSUS_WRONG_DELIMITER, CENSUS_WRONG_HEADER;
     }
 
     public ExceptionType type;
