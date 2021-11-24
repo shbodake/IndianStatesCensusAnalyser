ad71ffe369b3d030d00819ef4f7e570d5a887a93
shbodake <sayalibodake28@gmail.com>
2021-11-24 15:43:18 +0530

    Added: Analyser to load indian states census information from csv

--- src/main/java/com/blp/stateanalyser/CensusAnalyser.java
+++ src/main/java/com/blp/stateanalyser/CensusAnalyser.java
@@ -1,4 +1,34 @@
 package com.blp.stateanalyser;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
 
 public class CensusAnalyser {
+    public int loadIndiaCensusData(String csvFilePath) {
+        Reader reader = null;
+        try {
+            reader = Files.newBufferedReader(Paths.get(csvFilePath));
+            CsvToBean<IndiaCensusCSV> csvToBean = new CsvToBeanBuilder<IndiaCensusCSV>(reader)
+                    .withType(IndiaCensusCSV.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+            // iterator doesn't consume memory
+            Iterator<IndiaCensusCSV> iterator = csvToBean.iterator();
+            int numOfEntries = 0;
+            while (iterator.hasNext()) {
+                numOfEntries++;
+                iterator.next();
+            }
+            return numOfEntries;
+        } catch (
+                IOException e) {
+            System.out.println(e);
+        }
+        return 0;
+    }
 }
---
+++ src/main/java/com/blp/stateanalyser/IndiaCensusCSV.java
@@ -0,0 +1,28 @@
+package com.blp.stateanalyser;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class IndiaCensusCSV {
+    @CsvBindByName(column = "State")
+    public String state;
+
+    @CsvBindByName(column = "Population")
+    public String population;
+
+    @CsvBindByName(column = "AreaInSqKm")
+    public String areaInSqKm;
+
+    @CsvBindByName(column = "DensityPerSqKm")
+    public String densityPerSqKm;
+
+
+    @Override
+    public String toString() {
+        return "IndiaCensusCSV{" +
+                "state='" + state + '\'' +
+                ", population='" + population + '\'' +
+                ", areaInSqKm='" + areaInSqKm + '\'' +
+                ", densityPerSqKm='" + densityPerSqKm + '\'' +
+                '}';
+    }
+}
